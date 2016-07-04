970dc5f30ee9982463a9809bfdece9317544794f
Shimakage <shimakage@gclue.jp>
2016-07-04 19:39:26 +0900

    add library

---
+++ example/barometer.py
@@ -0,0 +1,27 @@
+# coding: utf-8
+## @package FaBoBarometer_MPL115
+#  This is a library for the FaBo Barometer I2C Brick.
+#
+#  http://fabo.io/204.html
+#
+#  Released under APACHE LICENSE, VERSION 2.0
+#
+#  http://www.apache.org/licenses/
+#
+#  FaBo <info@fabo.io>
+
+
+import FaBoBarometer_MPL115
+import time
+
+mpl115 = FaBoBarometer_MPL115.MPL115()
+
+while True:
+    data  = mpl115.readData()
+    a_hpa = mpl115.readHpa(212.0)
+
+    print "hpa  = ", data['hpa']
+    print "temp = ", data['temp']
+    print "hpa_aizu = ", a_hpa
+    print
+    time.sleep(1)
